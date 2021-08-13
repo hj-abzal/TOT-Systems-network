b69df166d21f2797de8e18eab027c7da94c36aef
suan <abzal008@mail.ru>
2021-08-13 12:12:10 +0300

    add favicon and title of project

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,10 +3,16 @@ import './App.css';
 import DevHeader from '../components/dev-header/DevHeader';
 import { DEV_VERSION } from '../config';
 import Routes from '../components/routes/Routes';
-
+import { Helmet } from "react-helmet";
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Planktonics v1.0</title>
+        <link rel="canonical" href="https://hj-abzal.github.io/TOT-Systems-network" />
+        <meta name="description" content="Planktonics v1.0 messenger app" />
+      </Helmet>
       {DEV_VERSION && <DevHeader />}
       <Routes />
     </div>
